b4548dd52381dfc8684af7971db5481d08d664c9
Jandakast <jandakast@jandakast.tv>
2020-12-03 16:56:08 -0500

    Update Vault to 1.6.0

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -1,9 +1,38 @@
-﻿$checksum = 'a906a5263acb8ef1ad7726cc1ae5105d468e0c0ba7308664a13c6f7d01627652'
-$checksum64 = '1ab014315e40374e58fd3474a37d5989d65936419e8d6311f4d530913f2378f5'
-$version = '1.0.3'
-$url = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_386.zip"
-$url64bit = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_amd64.zip"
+﻿$ErrorActionPreference = 'Stop'
 
-$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# DO NOT CHANGE THESE MANUALLY. USE update.ps1
+$url = 'https://releases.hashicorp.com/vault/1.6.0/vault_1.6.0_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/vault/1.6.0/vault_1.6.0_windows_amd64.zip'
+$checksum = '59779ef2bcb9988d853a4b5a64c18e9beffb297114f3509a9878d605739b84f5'
+$checksum64 = 'f4df525126885663a622478de34a9e6970985e29875b86cbb5a72860b7ab1a3f'
 
-Install-ChocolateyZipPackage -PackageName "vault" -Url "$url" -UnzipLocation "$unzipLocation" -Url64 "$url64bit" -ChecksumType 'sha256' -Checksum "$checksum" -Checksum64 "$checksum64"
+$unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\vault")) {
+  if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\vault\tools")) {
+    # clean old plugins and ignore files
+    Write-Host "Removing old vault plugins"
+    Remove-Item "$env:ChocolateyInstall\lib\vault\tools\vault-*.*"
+  }
+}
+else {
+  if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\vault")) {
+    if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\vault\tools")) {
+      # clean old plugins and ignore files
+      Write-Host "Removing old vault plugins"
+      Remove-Item "$env:ALLUSERSPROFILE\chocolatey\lib\vault\tools" -Include "vault-*.*"
+    }
+  }
+}
+
+$packageParams = @{
+  PackageName   = "vault"
+  UnzipLocation = $unzipLocation
+  Url           = $url
+  Url64         = $url64
+  Checksum      = $checksum
+  Checksum64    = $checksum64
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageParams
